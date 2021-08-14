25310066630d69d283f3c916bc6eed9dce0d76b2
Christopher Lundblade <chris.lundblade@gmail.com>
2021-08-14 13:26:16 -0500

    made html map, added some js and css too

--- js/wasteland.js
+++ js/wasteland.js
@@ -11,7 +11,27 @@ function statValueRoller(){
         return dTenOne+dTenTwo;
     }
 }
+function getDistance(locationObj, destinationObj){
+    //takes in objects with properties x & y
+    //returns the number of spaces between them
+    return Math.abs(locationObj.x - destinationObj.x) + Math.abs(locationObj.y - destinationObj.y);
+}
+function getLegalMoves(arrayOfAllMapSquares, characterLocationObject, characterMoveStat){
+    //this will get way more complicated with walls and difficult terrain but for now it's fine
+    let arrayOfSquaresThisCharacterCanMoveTo = [];
+    arrayOfAllMapSquares.forEach(function (specificMapSquare){
+        if(getDistance(characterLocationObject, specificMapSquare) <= characterMoveStat){
+            arrayOfSquaresThisCharacterCanMoveTo.push(specificMapSquare);
+        }
+    })
+    return arrayOfSquaresThisCharacterCanMoveTo;
+}
+
 var player = {
+    location: {
+        x: 3,
+        y: 1
+    },
     PunchGood: 25+statValueRoller(),
     ShootGood: 25+statValueRoller(),
     Toughness: 25+statValueRoller(),
@@ -24,19 +44,39 @@ var player = {
     get HP(){ delete this.HP;
         return this. HP = Math.floor(player.Toughness/10) + Math.floor(Math.random()*10) + Math.floor(Math.random()*10);
         },
+    get currentHP(){ delete this.currentHP;
+        return this.currentHP = Math.floor(player.Toughness/10) + Math.floor(Math.random()*10) + Math.floor(Math.random()*10);
+    },
     get meleeDamageBonus(){
         delete this.meleeDamageBonus;
         return this. meleeDamageBonus = Math.floor(player.PunchGood/10);
     }
 }
 console.log(player.HP);
+console.log(player.currentHP);
 console.log(player.Move);
 console.log(player.meleeDamageBonus);
 
 var nineMil = {
     magSize: 12,
     damage (){
-        return 1 + Math.floor(Math.random()+10)
+        let crit = 0;
+        let roll = Math.floor(Math.random()*10)
+        if(roll ===10){
+            crit = Math.floor(Math.random()*6)
+        }
+        return 1 + roll + crit;
     },
     multiAttack: 3
+}
+var knife = {
+    damage(characterPunchGoodBonus){
+        let crit = 0;
+        let roll = Math.floor(Math.random()*10)
+        if(roll ===10){
+            crit = Math.floor(Math.random()*6);
+            //TODO log damage and when there's a crit. Maybe that can go with the call?
+        }
+        return characterPunchGoodBonus + roll + crit;
+    }
 }
